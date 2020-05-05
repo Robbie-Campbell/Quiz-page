b16ff438bbcd292bab303902a7bd2a223d5872f4
Rab Campbell <56073739+LFCRab@users.noreply.github.com>
2020-05-05 17:19:46 +0100

    Some extra features

--- app.js
+++ app.js
@@ -1,26 +1,15 @@
 const song = document.querySelector(".song");
 const revealLetter = document.querySelector(".reveal");
-const output = document.querySelector(".append");
+const output = document.querySelector(".output");
 const input = document.querySelector(".input-word");
 const start = document.querySelector(".start");
 const title = document.querySelector(".title");
 let Stringletters = "";
 let i = 0;
 
-window.onload= () =>{
-    title.classList.add("titleOnLoad")
-}
-
-function getFirstLetters() 
-{
-    input.addEventListener("click", ()=>{
-        Stringletters = song.value;
-        song.value = "";
-        console.log(Stringletters);
-    })
-    revealLetter.addEventListener("click", ()=>{
-            output.classList.add("appear");
-            output.append(Stringletters[i]);
+function letterAdd(){
+    output.classList.add("appear");
+            output.append(Stringletters[i] + " ");
             i++;
             var seconds = 0;
             setInterval(function () {
@@ -34,7 +23,34 @@ function getFirstLetters()
             if(Stringletters.length == i)
             {
                 revealLetter.classList.add("disabled");
+                Stringletters = "";
             }
+}
+
+window.onload= () =>{
+    title.classList.add("titleOnLoad")
+}
+
+function getFirstLetters() 
+{
+    input.addEventListener("click", ()=>{
+        Stringletters = song.value;
+        song.value = "";
+        console.log(Stringletters);
+    })
+    document.body.onkeyup = function(e){
+        if(e.keyCode == 32 ){
+            if(Stringletters.length > i)
+            {
+                letterAdd();
+            }
+        }
+        else{
+            return;
+        }
+    }
+    revealLetter.addEventListener("click", ()=>{
+            letterAdd();
     })
 }
 
