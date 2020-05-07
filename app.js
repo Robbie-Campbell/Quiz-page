ca374d25d297af09cb295e48b23ab99ad8c714b8
Rab Campbell <56073739+LFCRab@users.noreply.github.com>
2020-05-07 16:03:07 +0100

    Now has a sound effect for each letter

--- app.js
+++ app.js
@@ -7,9 +7,15 @@ const title = document.querySelector(".title");
 let Stringletters = "";
 let i = 0;
 
+function play(){
+    let audio = new Audio("./hitmarker_2 (1).mp3");
+    audio.play();
+}
+
 function letterAdd(){
     output.classList.add("appear");
-            output.append(Stringletters[i] + " ");
+            output.append(Stringletters[i] + "\n");
+            play();
             i++;
             var seconds = 0;
             setInterval(function () {
@@ -18,6 +24,7 @@ function letterAdd(){
             {
                 output.classList.remove("appear");
                 clearInterval(seconds);
+                
             }
              200});
             if(Stringletters.length == i)
